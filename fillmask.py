ef0dce80fb4050026fca35fb9febd8a3a4b14a13
YU HAN GOH <gohyuhan12345@gmail.com>
2022-09-12 22:32:29 +0900

    Update fillmask.py

--- fillmask.py
+++ fillmask.py
@@ -3,6 +3,10 @@ import os
 import cv2
 
 def fillMask(label):
+    
+    '''
+    pixel classes
+    '''
     urban_land = (0, 255, 255)
     argriculture_land = (255, 255, 0)
     range_land = (255, 0, 255)
@@ -10,8 +14,19 @@ def fillMask(label):
     water = (0, 0, 255)
     barren_land = (255, 255, 255)
     unknown = (0, 0, 0)
-
+    
+     '''
+     replace all pixel that is not equal to the rgb value stated to a certain rgb value,
+     
+     rgb(255,255,0) was the value replaced by the code below
+     '''
     label[np.all(label!=(argriculture_land and range_land and forest_land and water and barren_land ) ,axis=-1)] = (255,255,0)
+    
+    '''
+    to check if their are any pixel which doesn't have an cetain rgb value that represent a class stated above
+    will return [] if all pixel in the image have an cetain rgb value that represent a class stated above
+    '''
+    
     left = label[np.all(label!=(urban_land and argriculture_land and range_land and forest_land and water and barren_land ) ,axis=-1)]
     print(left)
     return label
